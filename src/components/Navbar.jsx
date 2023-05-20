62693880252cb2745f26139d365855916771eb7c
David Masek <masd04@vse.cz>
2023-05-20 20:14:28 +0200

    mobileNavFix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,38 @@
 import React from "react";
 import {Link} from 'react-router-dom';
 import { close, logo, menu, logoNapis, logoBezNapis, logoText} from "../static";
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 
 
 const Navbar = () => {
-  const [toggle, setToggle] = useState
-  (false);
+  const [toggle, setToggle] = useState(false);
+  const navRef = useRef();
+
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (!navRef.current.contains(event.target)) {
+        if (!toggle) return;
+        setToggle(false);
+      }
+    };
+
+    window.addEventListener('click', handleOutsideClick);
+
+    return () => {
+      window.removeEventListener('click', handleOutsideClick);
+    };
+  }, [toggle]);
+
+  const handleClick = (e) => {
+    e.stopPropagation();
+    setToggle((prev) => !prev);
+  }
+
+
   return (
                                                                               //  Atribut rounded odebrat.. ??
-    <nav className="w-full flex py-2 items-center xs:py-1 navbar top-0 bg-primary rounded-3xl">
+    <nav ref={navRef} className="w-full flex py-2 items-center xs:py-1 navbar top-0 bg-primary rounded-3xl">
 
         <a href="/FBCSmichov1/" className="hidden sm:flex md:flex lg:flex xl:flex w-1/12 xs:ml-5 ss:ml-5 sm:ml-5 md:ml-10 object-contain cursor-pointer scale-100 hover:scale-110"><img src={logoBezNapis} alt="Logo"/></a>
         <a href="/FBCSmichov1/" className="w-60 object-contain cursor-pointer sm:hidden"><img src={logoText} alt="Logo"/></a>
@@ -27,10 +49,10 @@ const Navbar = () => {
         </ul> 
 
                 {/* LOGO -> Nápis FBC? */}
-        <div className="sm:hidden flex flex-1 items-center justify-end">  
-              <img src={toggle ? close : menu} alt="MenuButton" className="w-[28px] h-[28px] object-contain mr-5"
-              onClick={() => setToggle((prev) => !prev)}
-              />
+          <div className="sm:hidden flex flex-1 items-center justify-end">  
+            <img src={toggle ? close : menu} alt="MenuButton" className="w-[28px] h-[28px] object-contain mr-5"
+            onClick={handleClick}
+           />
 
               <div className={`${toggle ? 'flex' : 'hidden'}
               p-6 bg-black-gradient absolute top-10 right-0 mx-4 my-7 min-w[140px] rounded-xl sidebar`}>
