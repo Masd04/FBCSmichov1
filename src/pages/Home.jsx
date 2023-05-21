378cacc28a1753501f110f7bba723d1802fb32d4
David Masek <masd04@vse.cz>
2023-05-22 00:03:37 +0200

    tdd

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,26 @@
 import React from 'react'
 import { florbalisti } from '../static'
+import {Link} from 'react-router-dom';
+import styles from '../style';
 
 function Home() {
   return (
     <section id="hero" className={`flex md:flex-row flex-col`}>
 
+		
 	<div className="container max-w-6xl p-6 mx-auto space-y-6 sm:space-y-12">
+	
 
-		<a rel="noopener noreferrer" href="/FBCSmichov1/clanek" className="block rounded-3xl max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 bg-gray-300">
-			<img src={florbalisti} alt="" className="object-cover w-full h-64 rounded-l-3xl sm:h-96 lg:col-span-7 dark:bg-gray-500" />
-			<div className="p-6 space-y-2 lg:col-span-5">
-				<h3 className="text-2xl font-semibold sm:text-4xl group-hover:underline group-focus:underline">Článek o florbalistech</h3>
-				<span className="text-xs dark:text-gray-400">February 19, 2021</span>
-				<p>Ei delenit sensibus liberavisse pri. Quod suscipit no nam. Est in graece fuisset, eos affert putent doctus id.</p>
-			</div>
-		</a>
+	<Link to="/clanek" className="block rounded-3xl max-w-sm gap-3 mx-auto sm:max-w-full hover:bg-gray-400 lg:grid lg:grid-cols-12 bg-gray-300">
+    <img src={florbalisti} alt="" className="object-cover w-full h-64 rounded-l-3xl sm:h-96 lg:col-span-7 dark:bg-gray-500 pointer-events-none" />
+    <div className="p-6 space-y-2 lg:col-span-5 pointer-events-none">
+        <h3 className="text-2xl font-semibold sm:text-4xl">Článek o florbalistech</h3>
+        <span className="text-xs dark:text-gray-400">February 19, 2021</span>
+        <p>Ei delenit sensibus liberavisse pri. Quod suscipit no nam. Est in graece fuisset, eos affert putent doctus id.</p>
+    </div>
+</Link>
+
+
 		
 		<div className="grid justify-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
 
