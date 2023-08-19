f75a017ff914674d07814d781b446a1975fdc155
David Masek <masd04@vse.cz>
2023-08-20 01:55:46 +0200

    wannado

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -58,12 +58,15 @@ const Navbar = () => {
     <div className="hidden sm:block absolute top-0 left-0 w-full h-16 bg-fbcyellow">
     <a href="https://www.ceskyflorbal.cz/" target="_blank" className="flex items-start justify-end h-full">
       <div className="bg-black px-6 py-1 mr-40 mt-[0.3rem] rounded-xl flex items-center scale-100 hover:scale-105 shadow-xl z-10">
-        <img src="https://lista.ceskyflorbal.cz/img/svg-src/cesky-florbal-logo.svg" alt="Český Florbal" className="h-8 mx-2 bg-white rounded-full" />
+        <img src="https://lista.ceskyflorbal.cz/img/svg-src/cesky-florbal-logo.svg" alt="Český Florbal" className="h-[2rem] mx-2 bg-white rounded-full" />
         <span className="text-white font-serif font-bold text-xl">Český Florbal</span>
         </div>
     </a>
-
-
+    <a href="https://www.wannadosports.com/wannado/" target="_blank" className="flex items-start justify-end h-full mr-[15rem]">
+      <div className="bg-black px-2 py-2 mr-44 mt-[-3.68rem] rounded-xl flex items-center scale-100 hover:scale-105 shadow-xl z-10">
+        <img src="https://www.wannadosports.com/wp-content/uploads/2018/06/wannado-white-cropped.png" alt="Wannado" className="h-[1.5rem] mx-2" />
+        </div>
+    </a>
 
   </div>
 
